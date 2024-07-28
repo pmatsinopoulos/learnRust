719d32ad63afd05f6b569f93182b7e61c99dc6a9
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-07-28 18:23:02 +0300

    Conditional build based on features

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -4,4 +4,9 @@ fn main() {
     println!("Hello, world!");
     let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
     println!("Did our date match? {}", re.is_match("2024-01-01"));
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "foo")] {
+            println!("foo");
+        }
+    }
 }
