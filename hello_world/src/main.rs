f95d29d493a1ad3d5be96a46ce7db0ab6b744719
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-07-29 09:30:03 +0300

    Read run-time environment variable

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use std::env;
 
 fn main() {
     println!("Hello, world!");
@@ -10,6 +11,16 @@ fn main() {
         }
     }
     // example of reading an environment variable:
+    // This environment variable is available at compile time.
     let version = env!("CARGO_PKG_VERSION");
     println!("Version = {}", version);
+
+    // There are environment variables which are not available at compile time, but
+    // only at run time. So, the following code does not compile
+    //
+    // let foo_bar = env!("FOO_BAR"); //<-------- does not compile
+    //
+    // The proper way to read this is:
+    let foo_bar = env::var("FOO_BAR");
+    println!("FOO_BAR *{:?}*", foo_bar);
 }
