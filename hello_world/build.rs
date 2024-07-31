a95d8231a6f85b947366b3ebb3aa86b103c90d26
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-07-31 08:05:55 +0300

    Print environment variable in build script

--- hello_world/build.rs
+++ hello_world/build.rs
@@ -1,8 +1,12 @@
 use cc;
+use std::env;
 
 fn main() {
     // Tell Cargo, if the given file changes to re-run the build script
     println!("cargo::rerun-if-changed=src/hello.c");
     // Use the "cc" crate to build a C file and statically linked to it
     cc::Build::new().file("src/hello.c").compile("hello");
+
+    let out_dir = env::var("OUT_DIR");
+    println!("Out dir = *{:?}*", out_dir); // note: These lines will be printed only if cargo build is invoked with "-vv".
 }
