1168b35baac7f5bf3323d8aeac7103a007f1d6d7
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-07-31 08:27:53 +0300

    Just a change in the main code to see whether the build.rs script will run or not

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -7,7 +7,7 @@ extern "C" {
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world Starting!");
     let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
     println!("Did our date match? {}", re.is_match("2024-01-01"));
     cfg_if::cfg_if! {
